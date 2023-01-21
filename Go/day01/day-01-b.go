35b138be4d5eaf004e14a83ffbda4d341111dcfe
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-01-21 17:57:40 +0300

    Update

---
+++ Go/day01/day-01-b.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	bytes, err := os.ReadFile("./input.txt")
+	if err != nil {
+		panic(1)
+	}
+	text := string(bytes)
+
+	cals := calsToSum(strings.Split(text, "\n\n"))
+
+	total := 0
+
+	for i := 0; i < 3; i++ {
+		total += cals[i]
+	}
+
+	fmt.Println(total)
+}
+
+func calsToSum(calories []string) []int {
+	cals := make([]int, len(calories))
+
+	for i, cal := range calories {
+		sum := 0
+		for _, c:= range strings.Split(cal, "\n") {
+			conv, _ := strconv.Atoi(string(c))
+			sum += conv
+		}
+		cals[i] = sum
+	}
+
+	sort.Slice(cals, func(i, j int) bool { return cals[i] > cals[j]})
+
+	return cals
+}
