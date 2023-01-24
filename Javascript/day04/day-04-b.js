be0a4bebc40aea18a29dcf282633bb00159d39f3
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-01-24 11:10:33 +0300

    Added day-04-b

---
+++ Javascript/day04/day-04-b.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+const text = fs.readFileSync("./input.txt", "utf8").trimEnd();
+
+let total = 0;
+
+for (let pair of text.split("\n")) {
+  let [first, second] = pair.split(",");
+  let [x1, x2] = first.split("-");
+  let [y1, y2] = second.split("-");
+
+  if (isSubset(+x1, +x2, +y1, +y2)) {
+    total++;
+  }
+}
+
+console.log(total);
+
+function isSubset(x1, x2, y1, y2) {
+  return x1 <= y2 && y1 <= x2;
+}
--- Rust/day04/day_04_b/src/main.rs
+++ Rust/day04/day_04_b/src/main.rs
@@ -1,3 +1,38 @@
+use std::fs;
+
+struct Range {
+    start: i32,
+    end: i32,
+}
+
+impl Range {
+    fn from_str(s: &str) -> Self {
+        let (start, end) = s.split_once("-").unwrap();
+        let start = start.parse::<i32>().unwrap();
+        let end = end.parse::<i32>().unwrap();
+
+        Range { start, end }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let text = fs::read_to_string("./input.txt").unwrap();
+
+    let mut total = 0;
+
+    for pair in text.lines() {
+        let (first, second) = pair.split_once(",").unwrap();
+        let first = Range::from_str(first);
+        let second = Range::from_str(second);
+
+        if is_subset(&first, &second) {
+            total += 1;
+        }
+    }
+
+    println!("{}", total);
+}
+
+fn is_subset(first: &Range, second: &Range) -> bool {
+    first.start <= second.end && first.end >= second.start
 }
